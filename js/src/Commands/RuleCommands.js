42b25e7c58d1a241c562c3ed726af89f4b17a88d
John Grey <jgrey@ucsc.edu>
2015-12-24 01:36:01 -0800

    added d3 to function call of util.selectOrShare

--- js/src/Commands/RuleCommands.js
+++ js/src/Commands/RuleCommands.js
@@ -218,7 +218,7 @@ define(['d3','utils'],function(d3,util){
         }
         
         //container
-        var mainContainer = util.selectOrShare("mainContainer");
+        var mainContainer = util.selectOrShare("mainContainer",d3);
         
         //draw rule
         var rule = mainContainer.selectAll(".rule").data([cwdData],function(d){
