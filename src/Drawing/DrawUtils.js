d472166a8bca344c2c640a50c80c34d14dc72d5d
John Grey <jgrey@ucsc.edu>
2016-02-21 07:49:07 -0800

    rewrote drawing code, making it cleaner

--- src/Drawing/DrawUtils.js
+++ src/Drawing/DrawUtils.js
@@ -1,5 +1,5 @@
 define(['underscore','d3'],function(_,d3){
-
+    "use strict";
     var DrawUtils = {};
 
     /**
@@ -11,85 +11,6 @@ define(['underscore','d3'],function(_,d3){
         d3.selectAll(toCleanUp.join(", ")).remove();
     };
 
-    
-    /**
-       @function drawGroup Generic function to draw a group of vertical nodes
-       @param container The Container to draw the group inside of
-       @param className General class all subelements will describe themselves as
-       @param data array of nodes(?) used to populate the group
-       @param xLocation translation amount relative to the container
-       @param groupWidth the maximum width of the group to be drawn
-       @todo group together and annotate if there are too many nodes to display
-    */
-    DrawUtils.drawGroup = function(globalData,container,className,data,xLocation,groupWidth){
-        //console.log("drawing:",data);
-        var amtOfSpace, heightOfNode,
-            animationLength = 100;
-        if(data.length > 0){
-            amtOfSpace = (globalData.usableHeight - 100);
-            heightOfNode = (amtOfSpace - (data.length * 20)) / data.length;
-        }else{
-            amtOfSpace = (globalData.usableHeight - 100);
-            heightOfNode = (amtOfSpace - 20);
-        }
-        var boundGroup = container.selectAll("."+className)
-            .data(data,function(d,i){ return d.id; });
-
-        //exit selection
-        boundGroup.exit().selectAll("rect")
-            .transition()
-            .duration(animationLength)
-            .style("fill","red");
-
-        boundGroup.exit().selectAll("text").transition()
-            .style("opacity",0);
-        
-        boundGroup.exit().transition().delay(animationLength).remove();
-
-        //entry selection
-        var entryGroup = boundGroup.enter().append("g")
-            .classed(className, true)
-            .attr("transform","translate(" + xLocation + ",100)");
-
-        //create in the entry selection
-        entryGroup.append("rect")
-            .attr("width",0)
-            .attr("height",0)
-            .style("fill",globalData.colours.lightBlue)
-            .style("opacity",0)
-            .attr("rx",0)
-            .attr("ry",0);
-        
-
-        entryGroup.append("text")
-            .style("text-anchor","middle")
-            .style("fill","white")
-            .style("opacity",0);
-
-
-        //update selection
-        //transition to updated sizes etc
-        boundGroup.transition().delay(animationLength).attr("transform",function(d,i){
-            return "translate(" + xLocation + "," + (100 + (i * (heightOfNode + 20))) + ")";
-        });
-        
-        container.selectAll("."+className).selectAll("rect")
-            .transition().delay(animationLength*3).duration(animationLength)
-            .attr("width",groupWidth)
-            .attr("height",heightOfNode)
-            .attr("rx",10)
-            .attr("ry",10)
-            .style("opacity",1);
-
-        container.selectAll("."+className).selectAll("text")
-            .transition().delay(animationLength*3).duration(animationLength)
-            .attr("transform","translate(" + (groupWidth * 0.5) + "," +
-                  (heightOfNode * 0.5) + ")")
-            .text(function(d){ return d.id + " : " + d.name; })
-            .style("opacity",1);
-        
-        return boundGroup;
-    };
 
     /**
        @function pathExtraction
@@ -107,7 +28,6 @@ define(['underscore','d3'],function(_,d3){
         return path.reverse();
     };
 
-
     /**
        @method selectOrShare
        @purpose select or create a container
@@ -122,61 +42,12 @@ define(['underscore','d3'],function(_,d3){
             container = parent.append(type)
                 .attr("id",containerName);
             if(setupFunc !== undefined){
-                setupFunc(container);
+                setupFunc(container,containerName);
             }
         }
         return container;
     };
 
-    
-    /**
-       Draw a standardised object
-       of { name : a::string, values : [b::string] }
-     */
-    DrawUtils.drawStandardObject = function(standardData,container,object,i){
-        var numOfDescriptions = standardData.nodeDescriptions.length,
-            availableHeight = standardData.colHeight,
-            centreTransformX = standardData.colWidth * -0.4,
-            transformY = 0,
-            width = standardData.colWidth * 0.8,
-            currentElement = container,
-            textData = object.values === undefined ? [object.name] : [`| ${object.name} |`].concat(object.values),
-            textHeight = 15;
-
-
-        //Offset by everything above in this group
-        transformY = i === 0 ? 0 : d3.select(currentElement[0][0].previousElementSibling)[0][0].getBoundingClientRect().bottom -100;
-
-        container.attr('transform',`translate(0,${transformY})`);
-        
-        var rect = DrawUtils.createOrShare('enclosingRect',container,undefined,"rect")
-            .attr("height",10)
-            .attr("width",width)
-            .attr("transform",`translate(${centreTransformX},0)`)
-            .style("fill",standardData.globalData.colours.darkerBlue);
-
-        //add the text
-        container.selectAll("text").remove();
-        var boundTexts = container.selectAll("text").data(textData);
-        boundTexts.enter().append("text")
-            //.attr("transform",(d,i)=>`translate(0,${(i+1)*textHeight})`)
-            .text(d=>d)
-            .style('text-anchor','middle')
-
-        DrawUtils.wrapText(boundTexts,width);
-
-        var offset = 0;
-        boundTexts.each(function(d){
-            var cur = d3.select(this);
-                offset += d3.select(cur[0][0].previousElementSibling)[0][0].getBoundingClientRect().height + 5;
-            cur.attr('transform',`translate(0,${offset})`);
-        });
-        
-        //get the bounding box,
-        //apply to the enclosing rectangle:
-        rect.attr("height",container[0][0].getBoundingClientRect().height);
-
-    };    
 
     /**
        @function wrapText
@@ -214,6 +85,68 @@ define(['underscore','d3'],function(_,d3){
     };
 
 
+    //
+    DrawUtils.singleNode = function(container,nodeData,groupData){
+        //console.log("draw detail: ",container, nodeData);
+        var bound = container.selectAll(".nodeData").data(nodeData);
+        bound.enter().append("g").classed("nodeData",true);
+        bound.exit().remove();
+        DrawUtils.drawIndividualData(bound,groupData);
+
+        
+        //After all drawing, tidy
+        var offset = 0;
+        bound.each(function(d,i){
+            var g = d3.select(this);
+            var bbox = g[0][0].previousElementSibling.getBBox();
+            offset += i===0 ? groupData.nodeDataSeparator : bbox.height + groupData.nodeDataSeparator;
+            //console.log(g[0][0].previousElementSibling.getBBox());
+            g.attr("transform",`translate(0,${offset})`);
+        });
+    };
+
+    //
+    DrawUtils.drawIndividualData = function(containerSelection,groupData){
+        containerSelection.each(function(d){
+            var cur = d3.select(this);
+            //add the heightless rectangle
+            if(cur.select("rect").empty()){
+                cur.append("rect");
+            }
+            cur.select("rect")
+                .attr("width",groupData.colWidth)
+                .attr("height",0);
+
+            var textArray;
+            if(d.values !== undefined){
+                textArray = [`| ${d.name} |`].concat(_.values(d.values),["----"]);
+            }else{
+                textArray = [d.name];
+            }
+
+            var boundTexts = cur.selectAll("text").data(textArray);
+            boundTexts.exit().remove();
+            boundTexts.enter().append("text")
+                .style("text-anchor","middle");
+
+            boundTexts.text(e=>e);
+
+            DrawUtils.wrapText(boundTexts,groupData.colWidth);
+
+            //calculate positions
+            var offset = 0;
+            boundTexts.each(function(e,i){
+                var t = d3.select(this),
+                    bbox = t[0][0].previousElementSibling.getBBox();
+                offset += i===0 ? groupData.nodeDataSeparator : bbox.height + groupData.nodeDataSeparator;
+                t.attr("transform",`translate(0,${offset})`);
+            });
+            
+        });
+    };
+    
+
+    
     
     return DrawUtils;
 });
