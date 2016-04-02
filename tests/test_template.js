cfde869546d365c053ece5498cf4e7fc00b7d8e2
John Grey <jgrey@ucsc.edu>
2016-04-01 18:10:55 -0700

    fixed incorrect initial node number

--- tests/test_template.js
+++ tests/test_template.js
@@ -16,7 +16,7 @@ exports.ShellTests = {
     initTest : function(test){
         var shell = makeShell();
         test.ok(shell.root !== undefined);
-        test.ok(Object.keys(shell.allNodes).length === 3);
+        test.ok(Object.keys(shell.allNodes).length === 1);
         test.ok(shell.allRules.length === 0);
         test.ok(Object.keys(shell.allRulesByName).length === 0);
         test.ok(shell.cwd.id === shell.root.id);
