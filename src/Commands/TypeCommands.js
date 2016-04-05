f067597fe1050dfe4103ae73ebeb74d7fb466554
John Grey <jgrey@ucsc.edu>
2016-04-05 03:28:11 -0700

    empty type commands module for possible type system

---
+++ src/Commands/TypeCommands.js
@@ -0,0 +1,23 @@
+if(typeof defien !== 'function'){
+    var define = require('amdefine')(module);
+}
+
+define(['../utils','underscore'],function(util,_){
+    "use strict";
+
+    var TypeCommands = {
+        "help" : function(globalData,values){
+
+        },
+        "draw" : function(globalData, values){
+
+        },
+        //define a Link Type or a Relation Type
+        "define" : function(globalData,values){
+            //type / relation
+        },
+        
+    };
+    
+    return TypeCommands;
+});
