089c7d3b12ed5d0b7184e746511eaf1840685e5a
John Grey <jgrey@ucsc.edu>
2017-03-04 15:01:25 +0000

    added an initial stub test

---
+++ test/GraphNode_tests.js
@@ -0,0 +1,16 @@
+//To be used with mocha --require babel-polyfill --compilers js:babel-register
+//Import * as aModule from '../src/aModule';
+import * as chai from 'chai';
+import { GraphNode} from '../src/Node/GraphNode';
+
+let should = chai.should();
+
+describe ("GraphNode", function() {
+    it("exists", function() {
+        let aNode = new GraphNode();
+        should.exist(aNode);
+        aNode.should.have.property('id').equals(0);
+        aNode.should.have.property('name').equals('anon');
+    });
+    
+});
