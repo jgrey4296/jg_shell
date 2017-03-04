70924418716f437884385fdd33286d2faba31b1c
John Grey <jgrey@ucsc.edu>
2017-03-04 14:32:28 +0000

    adding in config files for webpack, babel, flow, eslint

---
+++ webpack.config.babel.js
@@ -0,0 +1,53 @@
+import webpack from 'webpack';
+import path from 'path';
+import FlowBabelWebpackPlugin from 'flow-babel-webpack-plugin';
+
+const ENV = process.env.NODE_ENV || 'development';
+
+module.exports = {
+    context: path.resolve(__dirname,'src'),
+    entry: './Shell.js',
+    
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'JGShell.js',
+        library: "JGShell",
+        libraryTarget: "umd"        
+    },
+    
+    externals: {
+        "lodash": {
+            commonjs: "lodash",
+            commonjs2: "lodash",
+            amd: "lodash",
+            root: "_",
+        },
+        // "rete": {
+        //     commonjs: "rete",
+        //     commonjs2: "rete",
+        //     amd: "rete",
+        //     root: "rete"
+        // }
+    },
+
+    resolve:{
+        modules: [ path.resolve(__dirname, "src"),
+                   path.resolve(__dirname,"libs"),
+                   "node_modules"
+                 ]
+    },
+    
+    module:{
+        rules:[
+            {test:/\.js$/, exclude: /node_modules/, enforce: 'pre', loader: 'eslint-loader'},
+            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+        ]
+    },
+
+    plugins: [
+        new FlowBabelWebpackPlugin(),
+        // new webpack.optimize.UglifyJsPlugin({
+        //     sourceMap: true
+        // })
+    ],
+};
