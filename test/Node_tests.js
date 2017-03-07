5e89dc3e91f751fde02a807eaf5990d02acce4cb
John Grey <jgrey@ucsc.edu>
2017-03-07 04:55:14 +0000

    added Node test stubs

---
+++ test/Node_tests.js
@@ -0,0 +1,46 @@
+//To be used with mocha --require babel-polyfill --compilers js:babel-register
+//Import * as aModule from '../src/aModule';
+import * as chai from 'chai';
+import { GraphNode} from '../src/Node/GraphNode';
+
+let should = chai.should();
+
+describe ("General Node Tests :", function() {
+
+    describe.skip("GraphNode:", function(){
+
+    });
+
+    describe.skip("Action Note:", function(){
+
+    });
+
+    describe.skip("Bookmark Node:", function(){
+
+    });
+
+    describe.skip("Condition Node:", function(){
+
+    });
+
+    describe.skip("Event Node:", function(){
+
+    });
+
+    describe.skip("FSM Node:", function(){
+
+    });
+
+    describe.skip("Institution Node:", function(){
+
+    });
+
+    describe.skip("Rule Node:", function(){
+
+    });
+
+    describe.skip("State Node:", function(){
+
+    });
+    
+});
