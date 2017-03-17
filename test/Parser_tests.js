1c0ea0038499a5edc658ac075b2feefa13f6968b
John Grey <jgrey@ucsc.edu>
2017-03-17 15:16:45 +0000

    added stub file for the parser tests

---
+++ test/Parser_tests.js
@@ -0,0 +1,38 @@
+//To be used with mocha --require babel-polyfill --compilers js:babel-register
+//Import * as aModule from '../src/aModule';
+import * as chai from 'chai';
+import * as P from '../src/PParse';
+
+let should = chai.should(),
+    expect = chai.expect;
+
+describe ("Shell Parser Tests", function() {
+
+    describe("Raw Parsing", function() {
+
+        before(function(){
+            //setup the parser
+        });
+
+        //Test each command individually
+        
+    });
+
+    describe("Parsing Interfaced with shell", function() {
+
+        beforeEach(function(){
+            //setup the shell
+        });
+
+        afterEach(function(){
+            //clear the shell
+        });
+
+        //test each command does the right thing
+        
+
+    });
+
+    
+    
+});
