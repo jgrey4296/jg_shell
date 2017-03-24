c4bfa11fadb985a48863f99f24676dde4b412ed9
John Grey <jgrey@ucsc.edu>
2017-03-24 20:29:26 +0000

    removed obsolete files

--- src/Node/Constructors.js
+++ src/Node/Constructors.js
@@ -1,14 +1,5 @@
 import _ from 'lodash';
 import { GraphNode } from './GraphNode';
-import { Rule } from './Rule';
-import { Condition } from './Condition';
-import { Action } from './Action';
-import { Institution } from './Institution';
-import { Bookmark } from './Bookmark';
-import { State } from './State';
-import { FSM } from './FSM';
-import { Event } from './Event';
-
 /**
     Aggregates the constructors toget
     @module Node/Constructors
@@ -16,14 +7,6 @@ import { Event } from './Event';
 
 const ctors = new Map([
     ["graphnode" , GraphNode],
-    ["rule"      , Rule],
-    ["condition" , Condition],
-    ["action"    , Action],
-    ["institution" , Institution],
-    ["bookmark" , Bookmark],
-    ["state" , State],
-    ["event" , Event],
-    ["fsm" , FSM]
 ]);
 
 /**
