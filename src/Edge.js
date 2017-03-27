f59358a8cc436572c961f801aa3a86fe93b80449
John Grey <jgrey@ucsc.edu>
2017-03-27 17:59:58 +0100

    updated edge to store edge details, and export/import json

--- src/Edge.js
+++ src/Edge.js
@@ -1,3 +1,5 @@
+import { EdgeData } from './Commands/CommandStructures';
+
 /**
    The Edge definition object
    //sourceDetails -> edgeDetails -> destDetails
@@ -5,15 +7,25 @@
 
 export default class Edge {
     constructor(sourceDetails,edgeDetails=null,destDetails){
+        if (!(sourceDetails instanceof EdgeData)){
+            throw new Error('Source Details need to be EdgeData');
+        }
+        if (!(edgeDetails instanceof EdgeData) && edgeDetails !== null){
+            throw new Error('Edge Details need to be edge data');
+        }
+        if (!(destDetails instanceof EdgeData)){
+            throw new Error('Dest Details need to be edge data');
+        }
+        
         //Each param is an object
         //only *requirement* is source and dest must have ids
         this.source = sourceDetails;
-        this.edge   = edgeDetails;
+        this.edge   = edgeDetails || new EdgeData();
         this.dest   = destDetails;
 
         if ( sourceDetails === undefined || destDetails === undefined){
             throw new Error('Edges must have details passed to them');
-        }        
+        }
         if (!('id' in this.source) || !('id' in this.dest)){
             throw new Error(`Edges must have ids`);
         }
@@ -23,9 +35,6 @@ export default class Edge {
     }
 }
 
-Edge.fromJSON = function(obj){
-    return new Edge(obj.source,obj.edge,obj.dest);
-};
 
 Edge.prototype.connectedTo = function(id){
     return this.source.id === id || this.dest.id === id;
@@ -46,5 +55,35 @@ Edge.prototype.idMatchesSource = function(id){
 
 Edge.prototype.idMatchesDestination = function(id){
     return this.dest.id === id;
+};
+
+
+//Saving and loading:
+Edge.prototype.toJSONCompatibleObj = function(){
+    let obj = {
+        sourceData: {
+            id: this.source.id,
+            tags: Array.from(this.source.tags),
+            vals: Array.from(this.source.vals)
+        },
+        edgeData: {
+            id: this.edge.id,
+            tags: Array.from(this.edge.tags),
+            vals: Array.from(this.edge.vals)
+        },
+        destData : {
+            id: this.dest.id,
+            tags: Array.from(this.dest.tags),
+            vals: Array.from(this.dest.vals)
+        }
+    };
+    return obj;
+};
+
+Edge.fromJSON = function(obj){
+    let sourceObj = new EdgeData(obj.sourceData.id, obj.sourceData),
+        edgeObj = new EdgeData(obj.edgeData.id, obj.edgeData),
+        destObj = new EdgeData(obj.destData.id, obj.destData);
+    return [sourceObj, edgeObj, destObj];
 };
 
